568421e90dbe2240f3369be6618ba84c79100344
syuntoku14 <syuntoku14@keio.jp>
2018-09-02 17:01:12 +0900

    Added user interface

--- src/go2goal_action_client.py
+++ src/go2goal_action_client.py
@@ -2,12 +2,21 @@
 
 import rospy
 import actionlib
+import sys
 from alphabot1.msg import Go2GoalAction, Go2GoalGoal, Go2GoalResult
+
+argv = sys.argv
+try:
+    goal_x = float(argv[1])
+    goal_y = float(argv[2])
+except:
+    print("goal's type isn't float")
+    quit()
 
 rospy.init_node('alphabot1_go2goal_client')
 client = actionlib.SimpleActionClient('go2goal', Go2GoalAction)
 client.wait_for_server()
-goal = Go2GoalGoal(goal_x=2.0, goal_y=1.0)
+goal = Go2GoalGoal(goal_x=goal_x, goal_y=goal_y)
 client.send_goal(goal)
 client.wait_for_result()
 print('goal at')
